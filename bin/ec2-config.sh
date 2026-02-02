8e13683330c4c42c5f17fc792cda52a72749eda6
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-02 15:19:22 +0100

    Create ec2-config.sh

---
+++ bin/ec2-config.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+sudo tee /root/.aws/config <<EOF
+[default]
+region = ${RegionName}
+output = ${OutputFormat}
+EOF
+sudo chmod 600 /root/.aws/config
+sudo apt update
+sudo apt upgrade -y
+sudo apt install awscli docker.io docker-compose -y
+sudo docker swarm init
+sudo usermod -aG docker ubuntu
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo apt-get install ./minikube_latest_amd64.deb -y
