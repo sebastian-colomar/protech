501e905538352662a8b7e0cb2f9cc2f35183c251
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-08 00:46:41 +0100

    Create ec2-config-rhel.sh

---
+++ bin/ec2-config-rhel.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+export RegionName=ap-south-1
+export OutputFormat=json
+
+mkdir --parents .aws
+tee .aws/config <<EOF
+[default]
+region = ${RegionName}
+output = ${OutputFormat}
+EOF
+sudo mkdir --parents /root/.aws
+sudo tee /root/.aws/config <<EOF
+[default]
+region = ${RegionName}
+output = ${OutputFormat}
+EOF
+sudo dnf update -y
+sudo dnf install unzip -y
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install --update
+aws --version
