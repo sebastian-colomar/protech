54fcdb3506df7069a96fe603cbbb892ce963dd9c
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-08 16:21:06 +0100

    Update ec2-config-rhel.sh

--- bin/ec2-config-rhel.sh
+++ bin/ec2-config-rhel.sh
@@ -15,8 +15,9 @@ region = ${RegionName}
 output = ${OutputFormat}
 EOF
 sudo dnf update -y
-sudo dnf install unzip -y
 sudo dnf install git -y
+sudo dnf install tmux -y
+sudo dnf install unzip -y
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install --update
