5ce40b1c16d983a910211cc8c518ef06ea049554
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 11:05:35 +0100

    Update and rename jumphost-04-certified-operators.sh to jumphost-04-operators.sh

--- upgrade/4.10_upgrade/bin/jumphost-04-operators.sh
+++ upgrade/4.10_upgrade/bin/jumphost-04-operators.sh
@@ -6,3 +6,12 @@ for pkg in ${PKGS_CERTIFIED}; do
    index_image_tar
    index_image_transfer
 done
+
+# REDHAT OPERATOR INDEX
+export RH_INDEX=redhat-operator-index
+for pkg in ${PKGS_REDHAT}; do
+   index_image_prune
+   index_image_download
+   index_image_tar
+   index_image_transfer
+done
