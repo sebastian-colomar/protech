b696442e9f465c8efc3519bd2644a674329571b7
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 22:13:10 +0100

    Update and rename 04-mirroring.sh to 04-operator_index.sh

--- upgrade/bin/jumphost/04-operator_index.sh
+++ upgrade/bin/jumphost/04-operator_index.sh
@@ -75,3 +75,24 @@ index_image_transfer() {
 }
 
 date
+
+# CERTIFIED OPERATOR INDEX
+export RH_INDEX=certified-operator-index
+for pkg in ${PKGS_CERTIFIED}; do
+   index_image_prune
+   index_image_download
+   index_image_tar
+   index_image_transfer
+done
+
+# REDHAT OPERATOR INDEX
+export RH_INDEX=redhat-operator-index
+for pkg in ${PKGS_REDHAT}; do
+   index_image_prune
+   index_image_download
+   index_image_tar
+   index_image_transfer
+done
+
+date
+date
