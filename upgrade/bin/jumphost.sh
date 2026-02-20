0ae18e1bdc97accc4dc1ab53a52c9d26d3a4db1f
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 22:14:29 +0100

    Update jumphost.sh

--- upgrade/bin/jumphost.sh
+++ upgrade/bin/jumphost.sh
@@ -6,6 +6,6 @@ export UPGRADE_BIN=${HOME}/protech/upgrade/bin
 
 source ${UPGRADE_BIN}/jumphost/00-export_vars.sh
 source ${UPGRADE_BIN}/jumphost/01-local_registry.sh
-source ${UPGRADE_BIN}/jumphost/02-ocp_release.sh
-source ${UPGRADE_BIN}/jumphost/03-mirroring.sh
-source ${UPGRADE_BIN}/jumphost/04-operators.sh
+source ${UPGRADE_BIN}/jumphost/02-cli.sh
+source ${UPGRADE_BIN}/jumphost/03-ocp_release.sh
+source ${UPGRADE_BIN}/jumphost/04-operator_index.sh
