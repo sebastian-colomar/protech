00801817618bd772e360d36aa3329154eb18dfaf
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 22:49:18 +0100

    Update upgrade.sh

--- upgrade/bin/upgrade.sh
+++ upgrade/bin/upgrade.sh
@@ -1,7 +1,7 @@
 set -eux
 
-source ${UPGRADE_PATH}/00-export_vars.sh
-source ${UPGRADE_PATH}/01-local_registry.sh
-source ${UPGRADE_PATH}/02-cli.sh
-source ${UPGRADE_PATH}/03-ocp_release.sh
-source ${UPGRADE_PATH}/04-operator_index.sh
+source ${full_path}/00-export_vars.sh
+source ${full_path}/01-local_registry.sh
+source ${full_path}/02-cli.sh
+source ${full_path}/03-ocp_release.sh
+source ${full_path}/04-operator_index.sh
