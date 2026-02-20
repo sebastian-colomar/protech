29007691cd92ec784bd452019f0efa25efb54786
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 11:05:51 +0100

    Delete upgrade/4.10_upgrade/bin/4.10_02_olm-2.7-redhat.sh

--- upgrade/4.10_upgrade/bin/4.10_02_olm-2.7-redhat.sh
+++
@@ -1,8 +0,0 @@
-# REDHAT OPERATOR INDEX
-export RH_INDEX=redhat-operator-index
-for pkg in ${PKGS_REDHAT}; do
-   index_image_prune
-   index_image_download
-   index_image_tar
-   index_image_transfer
-done
