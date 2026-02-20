5aa51b1c7bf292c1c4e1aad31ba2499aeb091a58
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 10:53:10 +0100

    Update 00-jumphost-00-export_vars.sh

--- upgrade/4.10_upgrade/bin/00-jumphost-00-export_vars.sh
+++ upgrade/4.10_upgrade/bin/00-jumphost-00-export_vars.sh
@@ -1,23 +1,54 @@
-   # 1.3. Set the required environment variables:
+# Set the required environment variables:
 
+export ARCH_CATALOG=amd64
 export ARCH_RELEASE=x86_64
+
 export BINARIES="oc opm"
 export BINARY_PATH=${HOME}/bin
+
+export CONTAINER_IMAGE=docker.io/library/registry
+export CONTAINER_IMAGE_TAG=2.7
+export CONTAINER_NAME=registry
+export CONTAINER_PORT=5000
+export CONTAINER_VOLUME=/var/lib/registry
+
 #export LOCAL_SECRET_JSON=${XDG_RUNTIME_DIR}/containers/auth.json
 #export LOCAL_SECRET_JSON=${HOME}/.docker/config.json
 export LOCAL_SECRET_JSON=${HOME}/pull-secret.json
+
 export MIRROR_HOST=mirror.hub.sebastian-colomar.com
 export MIRROR_PORT=5000
 export MIRROR_PROTOCOL=http
+
 export OCP_RELEASE_NEW=4.10.64
 export OCP_RELEASE_OLD=4.9.59
+
 export OCP_REPOSITORY=ocp-release
 export PACKAGES="openshift-client opm"
+
+# PKGS_CERTIFIED contains the operators from the certified-operator-index
+# "ako-operator" is just an example for testing purposes, simulating an external operator such as IBM operators
+export PKGS_CERTIFIED='ako-operator'
+# PKGS_REDHAT contains the operators from the redhat-operator-index
+export PKGS_REDHAT='cluster-logging elasticsearch-operator local-storage-operator ocs-operator odf-operator'
+
 export PRODUCT_REPO=openshift-release-dev
 export RELEASE_NAME=ocp-release
 export REMOTE_USER=ec2-user
 export REMOVABLE_MEDIA_PATH=/mnt/mirror
+
+export RH_INDEX_LIST='certified-operator-index redhat-operator-index'
+
+export RH_INDEX_VERSION_NEW=v4.10
+export RH_INDEX_VERSION_OLD=v4.9
+
+export RH_REGISTRY=registry.redhat.io
+export RH_REPOSITORY=redhat
+
 export SSH_KEY=${HOME}/key.txt
 
+# These variables are derived from the previous ones:
+export CONTAINERS_STORAGE_CONF=${REMOVABLE_MEDIA_PATH}/containers/storage.conf
 export LOCAL_REGISTRY=${MIRROR_HOST}:${MIRROR_PORT}
 export MIRROR_OCP_REPOSITORY=mirror-${OCP_REPOSITORY}
+export TMPDIR=${REMOVABLE_MEDIA_PATH}/containers/cache
