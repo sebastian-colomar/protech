4d3cc6a5c3ef1f022b30255a446f905ae281bb57
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 02:08:27 +0100

    Update 4.10_02_olm-2.7-redhat.sh

--- upgrade/4.10_upgrade/bin/4.10_02_olm-2.7-redhat.sh
+++ upgrade/4.10_upgrade/bin/4.10_02_olm-2.7-redhat.sh
@@ -1,8 +1,8 @@
-   # REDHAT OPERATOR INDEX
-   export RH_INDEX=redhat-operator-index
-   for pkg in ${PKGS_REDHAT}; do
-      index_image_prune
-      index_image_download
-      index_image_tar
-      index_image_transfer
-   done
+# REDHAT OPERATOR INDEX
+export RH_INDEX=redhat-operator-index
+for pkg in ${PKGS_REDHAT}; do
+   index_image_prune
+   index_image_download
+   index_image_tar
+   index_image_transfer
+done
