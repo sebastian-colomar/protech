0df6d7c35f598e9c0b712d3449c5a26806e70885
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 02:07:56 +0100

    Update 4.10_02_olm-2.2.sh

--- upgrade/4.10_upgrade/bin/4.10_02_olm-2.2.sh
+++ upgrade/4.10_upgrade/bin/4.10_02_olm-2.2.sh
@@ -1,69 +1,69 @@
 
 
-   # 2.2. Allow unsigned registries for the Certified Operator Index:
+# 2.2. Allow unsigned registries for the Certified Operator Index:
 
-   sudo sed -i '/"registry.redhat.io": \[/i\
-               "registry.redhat.io/redhat/certified-operator-index": [\
-                   {\
-                       "type": "insecureAcceptAnything"\
-                   }\
-               ],' /etc/containers/policy.json
-
-
-   # 2.3. Run the source index image that you want to prune in a container:
-
-   for RH_INDEX in ${RH_INDEX_LIST}; do
-     podman run --authfile ${LOCAL_SECRET_JSON} -d --name ${RH_INDEX}-${RH_INDEX_VERSION_NEW} -p 50051 --rm ${RH_REGISTRY}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW}
-   done
+sudo sed -i '/"registry.redhat.io": \[/i\
+            "registry.redhat.io/redhat/certified-operator-index": [\
+                {\
+                    "type": "insecureAcceptAnything"\
+                }\
+            ],' /etc/containers/policy.json
 
 
-   # 2.4. Use the grpcurl command to get a list of the packages provided by the index:
+# 2.3. Run the source index image that you want to prune in a container:
 
-   for RH_INDEX in ${RH_INDEX_LIST}; do
-      node_port=$( podman port ${RH_INDEX}-${RH_INDEX_VERSION_NEW} | cut -d: -f2 )
-      podman run --rm --network host docker.io/fullstorydev/grpcurl:latest -plaintext localhost:${node_port} api.Registry/ListPackages | grep '"name"' | cut -d '"' -f4 | sort -u | tee ${REMOVABLE_MEDIA_PATH}/${RH_INDEX}-${RH_INDEX_VERSION_NEW}.txt
-   done
+for RH_INDEX in ${RH_INDEX_LIST}; do
+  podman run --authfile ${LOCAL_SECRET_JSON} -d --name ${RH_INDEX}-${RH_INDEX_VERSION_NEW} -p 50051 --rm ${RH_REGISTRY}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW}
+done
 
 
-   # 2.7. Run the following command to prune the source index of all but the specified packages and push the new index image to your target registry:
-   # 2.9. Run the following command on your workstation with unrestricted network access to mirror the content to local files:
-   # 2.10. Copy the directory that is generated in your current directory to removable media:
-   # 2.11. Transfer the generated tarball to the mirror host:
+# 2.4. Use the grpcurl command to get a list of the packages provided by the index:
 
-   ln -s ${BINARY_PATH}/oc-${OCP_RELEASE_NEW} ${BINARY_PATH}/oc-${RH_INDEX_VERSION_NEW}
-   ln -s ${BINARY_PATH}/opm-${OCP_RELEASE_NEW} ${BINARY_PATH}/opm-${RH_INDEX_VERSION_NEW}
+for RH_INDEX in ${RH_INDEX_LIST}; do
+   node_port=$( podman port ${RH_INDEX}-${RH_INDEX_VERSION_NEW} | cut -d: -f2 )
+   podman run --rm --network host docker.io/fullstorydev/grpcurl:latest -plaintext localhost:${node_port} api.Registry/ListPackages | grep '"name"' | cut -d '"' -f4 | sort -u | tee ${REMOVABLE_MEDIA_PATH}/${RH_INDEX}-${RH_INDEX_VERSION_NEW}.txt
+done
 
-   index_image_prune() {
-      export INDEX_IMAGE=${RH_REGISTRY}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW}   
-      export INDEX_IMAGE_PRUNED=localhost:${MIRROR_PORT}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW}
-      export INDEX_CONTAINER_NAME=${RH_INDEX}-${RH_INDEX_VERSION_NEW}-${pkg}
-      opm-${RH_INDEX_VERSION_NEW} index prune -f ${INDEX_IMAGE} -p "${pkg}" -t ${INDEX_IMAGE_PRUNED}  
-      podman push ${INDEX_IMAGE_PRUNED} --remove-signatures
-      podman run -d --name ${INDEX_CONTAINER_NAME} -p 50051 --replace --rm ${INDEX_IMAGE_PRUNED}
-      export node_port=$( podman port ${INDEX_CONTAINER_NAME} | cut -d: -f2 )
-      podman run --network host --rm docker.io/fullstorydev/grpcurl:latest -plaintext localhost:${node_port} api.Registry/ListPackages | grep '"name"' | cut -d '"' -f4 | sort -u | tee ${REMOVABLE_MEDIA_PATH}/${INDEX_CONTAINER_NAME}.txt
-   }
 
-   index_image_download() {
-      export MIRROR_OLM_REPOSITORY=mirror-${pkg}
-      export MIRROR_INDEX_REPOSITORY=${MIRROR_OLM_REPOSITORY}-${RH_INDEX_VERSION_NEW}
-      mkdir -p ${REMOVABLE_MEDIA_PATH}/${MIRROR_OLM_REPOSITORY}-${RH_INDEX_VERSION_NEW}
-      cd ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}
-      oc-${RH_INDEX_VERSION_NEW} adm catalog mirror ${INDEX_IMAGE_PRUNED} file://${MIRROR_INDEX_REPOSITORY} -a ${LOCAL_SECRET_JSON} --index-filter-by-os=linux/${ARCH_CATALOG} --insecure
-   }
+# 2.7. Run the following command to prune the source index of all but the specified packages and push the new index image to your target registry:
+# 2.9. Run the following command on your workstation with unrestricted network access to mirror the content to local files:
+# 2.10. Copy the directory that is generated in your current directory to removable media:
+# 2.11. Transfer the generated tarball to the mirror host:
 
-   index_image_tar() {
-      cd ${REMOVABLE_MEDIA_PATH}
-      tar cfv ${MIRROR_INDEX_REPOSITORY}.tar ${MIRROR_INDEX_REPOSITORY}     
-   }
+ln -s ${BINARY_PATH}/oc-${OCP_RELEASE_NEW} ${BINARY_PATH}/oc-${RH_INDEX_VERSION_NEW}
+ln -s ${BINARY_PATH}/opm-${OCP_RELEASE_NEW} ${BINARY_PATH}/opm-${RH_INDEX_VERSION_NEW}
 
-   index_image_transfer() {
-      export MIRROR_HOST=mirror.sebastian-colomar.com
-      tar cfv ${MIRROR_INDEX_REPOSITORY}.tar ${MIRROR_INDEX_REPOSITORY}
-      export MIRROR_HOST=mirror.sebastian-colomar.com
-      ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOTE_USER}@${MIRROR_HOST} "sudo mkdir -p ${REMOVABLE_MEDIA_PATH}"
-      ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOTE_USER}@${MIRROR_HOST} "sudo chown -R ${REMOTE_USER}. ${REMOVABLE_MEDIA_PATH}"
-      scp -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}.tar ${REMOTE_USER}@${MIRROR_HOST}:${REMOVABLE_MEDIA_PATH}
-      scp -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOVABLE_MEDIA_PATH}/${CONTAINER_NAME}.tar ${REMOTE_USER}@${MIRROR_HOST}:${REMOVABLE_MEDIA_PATH}
-      ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOTE_USER}@${MIRROR_HOST} "sudo chown -R ${USER}. ${REMOVABLE_MEDIA_PATH}"     
-   }
+index_image_prune() {
+   export INDEX_IMAGE=${RH_REGISTRY}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW}   
+   export INDEX_IMAGE_PRUNED=localhost:${MIRROR_PORT}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW}
+   export INDEX_CONTAINER_NAME=${RH_INDEX}-${RH_INDEX_VERSION_NEW}-${pkg}
+   opm-${RH_INDEX_VERSION_NEW} index prune -f ${INDEX_IMAGE} -p "${pkg}" -t ${INDEX_IMAGE_PRUNED}  
+   podman push ${INDEX_IMAGE_PRUNED} --remove-signatures
+   podman run -d --name ${INDEX_CONTAINER_NAME} -p 50051 --replace --rm ${INDEX_IMAGE_PRUNED}
+   export node_port=$( podman port ${INDEX_CONTAINER_NAME} | cut -d: -f2 )
+   podman run --network host --rm docker.io/fullstorydev/grpcurl:latest -plaintext localhost:${node_port} api.Registry/ListPackages | grep '"name"' | cut -d '"' -f4 | sort -u | tee ${REMOVABLE_MEDIA_PATH}/${INDEX_CONTAINER_NAME}.txt
+}
+
+index_image_download() {
+   export MIRROR_OLM_REPOSITORY=mirror-${pkg}
+   export MIRROR_INDEX_REPOSITORY=${MIRROR_OLM_REPOSITORY}-${RH_INDEX_VERSION_NEW}
+   mkdir -p ${REMOVABLE_MEDIA_PATH}/${MIRROR_OLM_REPOSITORY}-${RH_INDEX_VERSION_NEW}
+   cd ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}
+   oc-${RH_INDEX_VERSION_NEW} adm catalog mirror ${INDEX_IMAGE_PRUNED} file://${MIRROR_INDEX_REPOSITORY} -a ${LOCAL_SECRET_JSON} --index-filter-by-os=linux/${ARCH_CATALOG} --insecure
+}
+
+index_image_tar() {
+   cd ${REMOVABLE_MEDIA_PATH}
+   tar cfv ${MIRROR_INDEX_REPOSITORY}.tar ${MIRROR_INDEX_REPOSITORY}     
+}
+
+index_image_transfer() {
+   export MIRROR_HOST=mirror.sebastian-colomar.com
+   tar cfv ${MIRROR_INDEX_REPOSITORY}.tar ${MIRROR_INDEX_REPOSITORY}
+   export MIRROR_HOST=mirror.sebastian-colomar.com
+   ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOTE_USER}@${MIRROR_HOST} "sudo mkdir -p ${REMOVABLE_MEDIA_PATH}"
+   ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOTE_USER}@${MIRROR_HOST} "sudo chown -R ${REMOTE_USER}. ${REMOVABLE_MEDIA_PATH}"
+   scp -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}.tar ${REMOTE_USER}@${MIRROR_HOST}:${REMOVABLE_MEDIA_PATH}
+   scp -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOVABLE_MEDIA_PATH}/${CONTAINER_NAME}.tar ${REMOTE_USER}@${MIRROR_HOST}:${REMOVABLE_MEDIA_PATH}
+   ssh -i ${SSH_KEY} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${REMOTE_USER}@${MIRROR_HOST} "sudo chown -R ${USER}. ${REMOVABLE_MEDIA_PATH}"     
+}
