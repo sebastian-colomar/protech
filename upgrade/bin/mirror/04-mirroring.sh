b0fb6341b70d687451f8ce0c925ae75abea03378
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 21:16:25 +0100

    Update 04-mirroring.sh

--- upgrade/bin/mirror/04-mirroring.sh
+++ upgrade/bin/mirror/04-mirroring.sh
@@ -32,32 +32,31 @@ oc-${OCP_RELEASE_OLD} apply -f ${REMOVABLE_MEDIA_PATH}/${MIRROR_OCP_REPOSITORY}/
 #3.11. Create the CatalogSource object by running the following command to specify the catalogSource.yaml file in your manifests directory:
 #3.12. Create the ImageContentSourcePolicy (ICSP) object by running the following command to specify the imageContentSourcePolicy.yaml file in your manifests directory:
 
-# CERTIFIED OPERATOR INDEX
-cd ${REMOVABLE_MEDIA_PATH}
-export RH_INDEX=certified-operator-index
-for pkg in ${PKGS_CERTIFIED}; do
+index_image_upload() {
    export MIRROR_INDEX_REPOSITORY=mirror-${pkg}-${RH_INDEX_VERSION_NEW}
    cd ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}
    oc-${RH_INDEX_VERSION_NEW} adm catalog mirror file://${MIRROR_INDEX_REPOSITORY}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW} ${LOCAL_REGISTRY}/${MIRROR_INDEX_REPOSITORY} --insecure
    sed -i 's|name: .*$|name: '${MIRROR_INDEX_REPOSITORY//./-}'|' ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_INDEX}-*/catalogSource.yaml
-   oc-${OCP_RELEASE_OLD} apply -f ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_INDEX}-*/catalogSource.yaml
+   export target=$( ls ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_INDEX}-*/catalogSource.yaml | tail -1 )
+   oc-${OCP_RELEASE_OLD} apply -f ${target}
    oc-${RH_INDEX_VERSION_NEW} adm catalog mirror ${LOCAL_REGISTRY}/${MIRROR_INDEX_REPOSITORY}/${RH_REPOSITORY}-${RH_INDEX}:${RH_INDEX_VERSION_NEW} ${LOCAL_REGISTRY}/${MIRROR_INDEX_REPOSITORY} --insecure --manifests-only
    sed -i 's|name: .*$|name: '${MIRROR_INDEX_REPOSITORY//./-}'|' ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_REPOSITORY}-${RH_INDEX}-*/imageContentSourcePolicy.yaml
-   oc-${OCP_RELEASE_OLD} apply -f ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_REPOSITORY}-${RH_INDEX}-*/imageContentSourcePolicy.yaml
-done   
+   export target=$( ls ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_REPOSITORY}-${RH_INDEX}-*/imageContentSourcePolicy.yaml | tail -1 )
+   oc-${OCP_RELEASE_OLD} apply -f ${target}
+}
+
+# CERTIFIED OPERATOR INDEX
+cd ${REMOVABLE_MEDIA_PATH}
+export RH_INDEX=certified-operator-index
+for pkg in ${PKGS_CERTIFIED}; do
+  index_image_upload
+done
 
 # REDHAT OPERATOR INDEX
 cd ${REMOVABLE_MEDIA_PATH}
 export RH_INDEX=redhat-operator-index
 for pkg in ${PKGS_REDHAT}; do
-   export MIRROR_INDEX_REPOSITORY=mirror-${pkg}-${RH_INDEX_VERSION_NEW}
-   cd ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}
-   oc-${RH_INDEX_VERSION_NEW} adm catalog mirror file://${MIRROR_INDEX_REPOSITORY}/${RH_REPOSITORY}/${RH_INDEX}:${RH_INDEX_VERSION_NEW} ${LOCAL_REGISTRY}/${MIRROR_INDEX_REPOSITORY} --insecure
-   sed -i 's|name: .*$|name: '${MIRROR_INDEX_REPOSITORY//./-}'|' ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_INDEX}-*/catalogSource.yaml
-   oc-${OCP_RELEASE_OLD} apply -f ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_INDEX}-*/catalogSource.yaml
-   oc-${RH_INDEX_VERSION_NEW} adm catalog mirror ${LOCAL_REGISTRY}/${MIRROR_INDEX_REPOSITORY}/${RH_REPOSITORY}-${RH_INDEX}:${RH_INDEX_VERSION_NEW} ${LOCAL_REGISTRY}/${MIRROR_INDEX_REPOSITORY} --insecure --manifests-only
-   sed -i 's|name: .*$|name: '${MIRROR_INDEX_REPOSITORY//./-}'|' ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_REPOSITORY}-${RH_INDEX}-*/imageContentSourcePolicy.yaml
-   oc-${OCP_RELEASE_OLD} apply -f ${REMOVABLE_MEDIA_PATH}/${MIRROR_INDEX_REPOSITORY}/manifests-${RH_REPOSITORY}-${RH_INDEX}-*/imageContentSourcePolicy.yaml
+  index_image_upload
 done
 
 date
