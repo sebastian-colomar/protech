51c8b92b74ca3ce6523f1df5f47b9aeedabacff5
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-20 02:08:11 +0100

    Update 4.10_02_olm-2.7-certified.sh

--- upgrade/4.10_upgrade/bin/4.10_02_olm-2.7-certified.sh
+++ upgrade/4.10_upgrade/bin/4.10_02_olm-2.7-certified.sh
@@ -1,8 +1,8 @@
-   # CERTIFIED OPERATOR INDEX
-   export RH_INDEX=certified-operator-index
-   for pkg in ${PKGS_CERTIFIED}; do
-      index_image_prune
-      index_image_download
-      index_image_tar
-      index_image_transfer
-   done
+# CERTIFIED OPERATOR INDEX
+export RH_INDEX=certified-operator-index
+for pkg in ${PKGS_CERTIFIED}; do
+   index_image_prune
+   index_image_download
+   index_image_tar
+   index_image_transfer
+done
