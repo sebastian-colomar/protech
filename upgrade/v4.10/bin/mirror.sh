2d87093e6e4d149cb29da5c00527c5ff8e78670c
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-21 15:18:47 +0100

    Update mirror.sh

--- upgrade/v4.10/bin/mirror.sh
+++ upgrade/v4.10/bin/mirror.sh
@@ -1,6 +1,5 @@
 set -eux
 
-source ${UPGRADE_FULL_PATH}/00-export_vars.sh
 source ${UPGRADE_FULL_PATH}/01-local_registry.sh
 source ${UPGRADE_FULL_PATH}/02-cli.sh
 source ${UPGRADE_FULL_PATH}/03-ocp_release.sh
