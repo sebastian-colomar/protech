20a51c8fe93f8077b7af7fecd2fcb9993dad22d5
Sebastian Colomar <sebastian.colomar@gmail.com>
2026-02-22 03:35:48 +0100

    Update mirror.sh

--- upgrade/v4.10/bin/mirror.sh
+++ upgrade/v4.10/bin/mirror.sh
@@ -1,6 +1,11 @@
 set -eux
+: "${HOST:?HOST must be defined}"
+: "${RELEASE:?RELEASE must be defined}"
 
-source ${FULL_PATH}/01-local_registry.sh
-source ${FULL_PATH}/02-cli.sh
-source ${FULL_PATH}/03-ocp_release.sh
-source ${FULL_PATH}/04-operator_index.sh
+export KUBECONFIG=${HOME}/auth/kubeconfig
+
+BIN=$(cd "$(dirname "$0")" && pwd)
+BIN_HOST=${BIN}/${HOST}
+
+source ${BIN}/values.sh
+source ${BIN_HOST}/*.sh
